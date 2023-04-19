fd51926760ca93b29ab4f6de3cc1c1f3a98c9ff9
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-04-19 16:49:03 +0200

    Complete fetchPostsHandler basic setup

--- libs/api/feed/feature/tests/fetch-posts.handler.spec.ts
+++ libs/api/feed/feature/tests/fetch-posts.handler.spec.ts
@@ -2,8 +2,10 @@ import { Test } from '@nestjs/testing';
 import { FetchPostsCommand, FetchPostsResponse } from '@mp/api/feed/util';
 import { FeedRepository } from '@mp/api/feed/data-access';
 import { FetchPostsHandler } from '../src/commands/';
+import { FilterList } from '../../util/src/interfaces/filter-list.interface';
+import { Discipline } from '../../util/src/enums/discipline.enum';
 
-describe('FetchPostsHandler', () => {
+describe('FetchPostsHandler Test ', () => {
   let fetchPostsHandler: FetchPostsHandler;
   let feedRepository: FeedRepository;
 
@@ -28,33 +30,39 @@ describe('FetchPostsHandler', () => {
     expect(fetchPostsHandler).toBeDefined();
   });
 
-  // it('should return a FetchPostsResponse object', async () => {
-  //   const filters = undefined;
-  //   const request = { filters };
-  //   const command = new FetchPostsCommand(request);
+  it('should return a FetchPostsResponse object', async () => {
+    const filters: FilterList = {
+      list: null,
+    };
+    const request = { filters };
+    const command = new FetchPostsCommand(request);
 
-  //   const mockPostsDoc = {
-  //     data: [
-  //       {
-  //         id: '1',
-  //         title: 'Test Post 1',
-  //         content: 'Test Content 1',
-  //       },
-  //     ],
-  //   };
+    const mockPostsDoc = {
+      data: [
+        {
+          id: '1',
+          title: 'Test Post 1',
+          author: null,
+          description: 'Test Description 1',
+          content: 'Test Content 1',
+          time: 1234567890,
+          discipline: Discipline.SCIENCE, // Replace with an actual value from the Discipline enum
+        },
+      ],
+    };
 
-  //   jest.spyOn(feedRepository, 'fetchPosts').mockResolvedValue(mockPostsDoc);
+    jest.spyOn(feedRepository, 'fetchPosts').mockResolvedValue(mockPostsDoc);
 
-  //   const expectedResponse: FetchPostsResponse = {
-  //     posts: {
-  //       postsFound: true,
-  //       list: mockPostsDoc.data,
-  //     },
-  //   };
+    const expectedResponse: FetchPostsResponse = {
+      posts: {
+        postsFound: true,
+        list: mockPostsDoc.data,
+      },
+    };
 
-  //   const result = await fetchPostsHandler.execute(command);
+    const result = await fetchPostsHandler.execute(command);
 
-  //   expect(feedRepository.fetchPosts).toHaveBeenCalledWith(filters);
-  //   expect(result).toEqual(expectedResponse);
-  // });
+    expect(feedRepository.fetchPosts).toHaveBeenCalledWith(filters);
+    expect(result).toEqual(expectedResponse);
+  });
 });
