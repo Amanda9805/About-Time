76294c62d75860ec434dc06e543022629313a44a
Luke Lawson <u21433811@tuks.co.za>
2023-04-20 12:07:26 +0200

    Implemented fetchUserPosts

--- libs/api/profiles/data-access/src/profiles.repository.ts
+++ libs/api/profiles/data-access/src/profiles.repository.ts
@@ -71,33 +71,58 @@ async deleteAccount(profile : IProfile) {
   async fetchUserPosts(userProfile: IProfile) {
     
 
-    const toReturn = {
-      data: [
-          {
-              id: "post 1",
-              title: "Burger King Foot Lettuce",
-              author: null,
-              description: "This is a very orginal and cool post!",
-              content: "Wow, I really am I a super cool story - pls spend time",
-              discipline: Discipline.SCIENCE,
-              time: 500
-          },
-          {
-              id: "post 1",
-              title: "Burger King Foot Lettuce",
-              author: null,
-              description: "This is a very orginal and cool post!",
-              content: "Wow, I really am I a super cool story - pls spend time",
-              discipline: Discipline.SCIENCE,
-              time: 500
-          }
-      ]
-    };
+    const userID = userProfile.userId;
+    const postIDs : string[] = [];
+    const postImages = new Map<string, string>();
+
+    const toReturn: { id: string; title: string; author: string; description: string; 
+      content: string; time: number; discipline: Discipline; image:string | undefined}[] = [];
+
+    const userPostDocument = await admin.firestore()
+    .collection("Posts")
+    .where("userId", "==", userID)
+    .orderBy("created", "desc")
+    .get();
+    
+    
+    
+    userPostDocument.forEach((userPost) => {
+        const data = userPost.data();
+        postIDs.push(data["id"])
+      });
+  
+
+    const postImageDocument = await admin.firestore()
+    .collection("PostPhotos")
+    .where("postId", "in", postIDs)
+    .get().then((postImageList) =>  {
+      postImageList.forEach((postImage) => {
+        const data = postImage.data();
+        postImages.set(data["postId"], data["image"])
+      })
+    });
+
+    userPostDocument.forEach((userPost) => {
+      const currentDoc = userPost.data();
+      const currentDocPostData = currentDoc['postDetails'];
+      toReturn.push({
+        id: currentDoc['id'],
+        title: currentDoc['title'],
+        author: userID,  // TODO: Create function to interpret ```currentDoc['author']``` 's userId value and fetch the appropriate user details
+        description: currentDocPostData['desc'],
+        content: currentDocPostData['content'],
+        time: currentDocPostData['timeWatched'],
+        discipline: this.interpretDiscipline(currentDocPostData['discipline']),   // TODO - done: Create function to interpret ```currentDocPostData['discipline']``` 's value
+        image : postImages.get(currentDoc["id"])
+      });
+    });
+
+    
 
     return {
       "postsFound": true, 
       // "list": toReturn.data
-      "list": null
+      "list": toReturn
     }
   }
 
@@ -110,6 +135,28 @@ async deleteAccount(profile : IProfile) {
     // Return success enum
     return Status.SUCCESS;
 
+
+  }
+
+
+  interpretDiscipline(disciplineStr: string) {
+    if (disciplineStr.toLowerCase() == "art") {
+      return Discipline.ART;
+    } else if (disciplineStr.toLowerCase() == "food") {
+      return Discipline.FOOD;
+    } else if (disciplineStr.toLowerCase() == "gaming") {
+      return Discipline.GAMING;
+    } else if (disciplineStr.toLowerCase() == "sport") {
+      return Discipline.SPORT;
+    } else if (disciplineStr.toLowerCase() == "science") {
+      return Discipline.SCIENCE;
+    } else if (disciplineStr.toLowerCase() == "news") {
+      return Discipline.NEWS;
+    } else if (disciplineStr.toLowerCase() == "travel") {
+      return Discipline.TRAVEL;
+    } else {
+      return Discipline.MUSIC;
+    }
 
   }
 }
