b28fd4a0a9b190bc4b5fc12532b6a48ba1c634db
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-04-21 11:19:16 +0200

    search fix

--- libs/app/search/feature/src/search.module.ts
+++ libs/app/search/feature/src/search.module.ts
@@ -4,12 +4,12 @@ import { IonicModule } from '@ionic/angular';
 import { SearchPage } from './search.page';
 import { SearchRouting } from './search.routing';
 import { TimerModule } from '@mp/app/timer/feature';
-import { SearchApi } from '@mp/app/search/data-access';
+import { SearchApi, SearchState } from '@mp/app/search/data-access';
 import { NgxsModule } from '@ngxs/store';
 import { FeedState } from '@mp/app/feed/data-access';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, SearchRouting, TimerModule, NgxsModule.forFeature([FeedState])],
+  imports: [CommonModule, IonicModule, SearchRouting, TimerModule, NgxsModule.forFeature([SearchState])],
   declarations: [SearchPage],
   providers: [SearchApi],
 })
--- libs/app/search/util/src/search.actions.ts
+++ libs/app/search/util/src/search.actions.ts
@@ -1,6 +1,3 @@
-import { MinimizedProfile } from '@mp/api/search/util';
-import { ProfilesList } from '@mp/api/search/util';
-
 export class SetMinimizedProfile {
   static readonly type = '[Search] SetMinimizedProfile';
   constructor(public readonly error: string | null) { }
