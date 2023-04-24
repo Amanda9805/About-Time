19a586275ba92852c9a8c1724eb7b7d89add0bb4
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-04-24 20:37:04 +0200

    Settings Ui

--- libs/app/settings/feature/src/lib/settings.module.ts
+++ libs/app/settings/feature/src/lib/settings.module.ts
@@ -4,11 +4,12 @@ import { IonicModule } from '@ionic/angular';
 import { SettingsPage } from './settings.page';
 import { SettingsRouting } from './settings.routing';
 import { FormsModule } from '@angular/forms';
+import { VersionModule } from '@mp/app/version/ui';
 //import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
-  imports:[CommonModule, IonicModule, IonicModule.forRoot(), SettingsRouting, FormsModule],
+  imports: [CommonModule, IonicModule, IonicModule.forRoot(), SettingsRouting, FormsModule, VersionModule],
   declarations: [SettingsPage],
 })
-export class SettingsModule{}
+export class SettingsModule { }
