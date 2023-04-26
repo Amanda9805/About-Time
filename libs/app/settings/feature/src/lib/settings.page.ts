362de8e9120476f9299967556e3e4b31b77d4241
Jhindosh <dharshanpillay@gmail.com>
2023-04-26 14:17:10 +0200

    settings fixes

--- libs/app/settings/feature/src/lib/settings.page.ts
+++ libs/app/settings/feature/src/lib/settings.page.ts
@@ -4,6 +4,11 @@ import { Location } from '@angular/common';
 import { IonModal } from '@ionic/angular';
 import { OverlayEventDetail } from '@ionic/core/components';
 import { Router, NavigationExtras } from '@angular/router';
+import { ProfilesRepository } from '@mp/api/profiles/data-access';
+import { PrivacyStatus, Status } from '@mp/api/profiles/util';
+import { getPrivacySettings } from '@mp/api/core/feature';
+import { current } from 'immer';
+
 
 @Component({
   selector: 'app-settings',
@@ -11,8 +16,11 @@ import { Router, NavigationExtras } from '@angular/router';
   styleUrls: ['./settings.page.scss']
 })
 export class SettingsPage {
+  profileRepo = new ProfilesRepository();
+  toggle = document.getElementById('my-toggle');
 
-  privacy = false;
+//const user=; //ask how to get the current user
+ // currentPrivacy: PrivacyStatus;
   sliderValue = 50;
   @ViewChild(IonModal)
   modal!: IonModal;
@@ -21,7 +29,7 @@ export class SettingsPage {
   name!: string;
 
   constructor(public alertController: AlertController, private location: Location, private router: Router) {
-
+    //const user=; //ask how to get the current user
   }
 
   cancel() {
@@ -39,12 +47,53 @@ export class SettingsPage {
     }
   }
 
+  // async ngOnInit() {
+  //   this.currentPrivacy = await this.profileRepo.getPrivacySettings(user);
+  //   if (this.currentPrivacy === PrivacyStatus.PRIVATE) {
+  //     this.toggle?.ariaChecked;
+  //   }
+  // }
+
+  deleteAccount(){
+    // try{
+    //   //const profileRepo = new ProfilesRepository();
+    //   const result = this.profileRepo.deleteAccount();//figure out how to get the users profile
+    //   if(result == Status.SUCCESS)
+    //   {
+    //     console.log("Account deletion sucessful.")
+    //     this.router.navigate(['/welcome']);
+    //   }
+    //   else{
+    //     console.log("Account deletetion failed.")
+    //     alert('Failed to delete account');
+
+    //   }
+    // }
+    // catch(error){
+    //   console.error(error)
+    //   alert('Failed to delete account');
+    // }
+  }
   goBack() {
     this.location.back();
   }
 
   onToggleChange(event: any) {
     console.log('Toggle changed', event.detail.checked);
+  }
+
+  toggleProfileVisibility(){
+    //Need to get the user and then the function should be done but It still needs to be tested;
+    let newPrivacy;  
+    if(this.toggle?.ariaChecked)
+    {
+      newPrivacy = PrivacyStatus.PRIVATE
+    }
+    else{
+      newPrivacy = PrivacyStatus.PUBLIC;
+    }
+    const result = this.profileRepo.updatePrivacySettings(this.user,newPrivacy)
+    console.log(result);
   }
 
   onSliderChange(event: any) {
