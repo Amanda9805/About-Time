5bf94881e91126c64b458ef423a358fe85260390
FrancovV <74922600+FrancovV@users.noreply.github.com>
2023-04-27 13:08:25 +0200

    added live updates

--- libs/app/feed/ui/src/lib/feed-closed/feed-closed.component.ts
+++ libs/app/feed/ui/src/lib/feed-closed/feed-closed.component.ts
@@ -1,15 +1,16 @@
-import { Component, EventEmitter, Output, Input, ChangeDetectorRef, SimpleChanges, ChangeDetectionStrategy } from '@angular/core';
+import { Component, EventEmitter, Output, Input, ChangeDetectorRef, SimpleChanges, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { FilterList, FilterType, Post, PostList } from '@mp/api/feed/util';
+import { doc, docSnapshots, Firestore, collection, collectionChanges } from '@angular/fire/firestore';
+import {map} from 'rxjs';
 import { Store } from '@ngxs/store';
-import { ActionsExecuting, actionsExecuting } from '@ngxs-labs/actions-executing';
-import { Observable } from 'rxjs';
+import { AuthState } from '@mp/app/auth/data-access';
 
 @Component({
   selector: 'mp-feed-closed',
   templateUrl: './feed-closed.component.html',
   styleUrls: ['./feed-closed.component.scss']
 })
-export class FeedClosedComponent {
+export class FeedClosedComponent{
 
   @Input() posts: PostList = {
     postsFound: false,
@@ -20,7 +21,9 @@ export class FeedClosedComponent {
     list: [],
   };
 
-  constructor() {
+
+
+  constructor(private readonly firestore : Firestore, private store: Store) {
     this.filters.list?.push(
       FilterType.MOST_RECENT,
       FilterType.MOST_POPULAR,
@@ -32,12 +35,46 @@ export class FeedClosedComponent {
       FilterType.TRAVEL_FILTER,
       FilterType.MUSIC_FILTER,
       FilterType.GAMING_FILTER)
+
+
+
+
+      //const app = firestore.app;
+  }
+
+  async getUserEventSummary() {
+    try{
+      const user = this.store.selectSnapshot(AuthState).user.uid;
+
+      const ref = doc(this.firestore, 'profiles/', user);//fix path
+      console.log(ref.path);
+      console.log(docSnapshots(ref));
+      return docSnapshots(ref).pipe(map(data => data.data()));
+    }catch(e){
+      console.log(e);
+    }
+    return;
+  }
+
+  async test(){
+    const items = collection(this.firestore, 'profiles');
+    collectionChanges(items).subscribe(data => {
+      console.log(data);
+    });
+
+
+    const doc$ = await this.getUserEventSummary();
+
+    doc$?.subscribe(data => {
+      console.log(data);
+    });
   }
 
   @Output() filterChanged = new EventEmitter<FilterType>();
   @Output() setCurrentPost = new EventEmitter<Post>();
 
   onSetFilters(data: FilterType) {
+
     this.filterChanged.emit(data);
     const myElement = document.getElementById(data);
     if(myElement != null)
