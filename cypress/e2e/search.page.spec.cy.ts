98f4bea2d30b958378c3063e0095b2498121a831
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-05-02 10:37:58 +0200

    Fix search Cypress Test

--- cypress/e2e/search.page.spec.cy.ts
+++ cypress/e2e/search.page.spec.cy.ts
@@ -3,26 +3,30 @@ describe('Search Page Test', () => {
         cy.viewport(390, 844);
         cy.visit('http://localhost:4200/login');
         cy.wait(2000);
-    
+
         cy.location('pathname').then((current) => {
-          if(!current.includes('home/feed')) {
-            cy.get('ion-input[formControlName="email"]')
-            .find('input')
-            .type('test@test.com');
-    
-            cy.get('ion-input[formControlName="password"]')
-            .find('input')
-            .type('Testing123?');
-            cy.wait(250);
-    
-            cy.get('ion-button').contains("It's about time!").click();
-            cy.location('pathname').then((curr) => {
-              if(!curr.includes('home/feed')) {
-                cy.get('ion-button').contains("It's about time!").click();
-              }
-            });
+          if (!current.includes('home/feed')) {
+              cy.get('ion-input[formControlName="email"]')
+                  .find('input')
+                  .type('test@test.com');
+
+              cy.get('ion-input[formControlName="password"]')
+                  .find('input')
+                  .type('Testing123?');
+
+              cy.wait(250);
+
+              const loginButton = cy.get('ion-button').contains("Welcome. You're late!");
+              loginButton.click();
+              cy.location('pathname').then((curr) => {
+                  if (!curr.includes('home/feed')) {
+                      loginButton.click();
+                  }
+              });
+              cy.wait(2000);
           }
-        });
+      });
+
 
         cy.visit('http://localhost:4200/home/search');
     });
@@ -35,13 +39,13 @@ describe('Search Page Test', () => {
         cy.get('mp-timer').should('be.visible');
     });
 
-    
+
     it('Contains Search Bar', () => {
         cy.get('ion-searchbar').should('be.visible');
     });
 
     /*describe('Search Results Tests', () => {
-         will work on this later 
+         will work on this later
     });*/
 });
 
